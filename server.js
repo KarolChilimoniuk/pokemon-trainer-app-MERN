1634023b50347a71b666066c4c583a3ab4628373
Karol Chilimoniuk <webdevkarolchilimoniuk@gmail.com>
2022-11-06 13:13:56 +0100

    the same

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-// const cors = require("cors");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
