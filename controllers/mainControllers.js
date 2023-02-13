91b2b6033462bd723f453e0d258e3949aad9f234
Karol Chilimoniuk <webdevkarolchilimoniuk@gmail.com>
2023-02-13 23:09:19 +0100

    mod:..

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,8 +1,8 @@
 const getMain = async (req, res) => {
   try {
-    res.status(200).send("Hello World!");
+    res.send("Hello World!");
   } catch (err) {
-    res.status(500).send({ message: "Server error :(" });
+    res.send("Server error :(");
   }
 };
 
